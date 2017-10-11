9947fd282b720ec78c2121c8a9a288d5adb9fa37
ZL <2324216277@qq.com>
2017-10-11 17:35:28 +0800

    add file

---
+++ webside1/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Website1</title>
+</head>
+<body>
+	<ul>
+		<li><a href="index.php">index</a></li>
+		<li><a href="about.php">about</a></li>
+		<li><a href="contact.php">contact</a></li>
+	</ul>
+
+
---
+++ webside1/index.php
@@ -0,0 +1,14 @@
+<?php include "./inc/header.php" ?>
+<?php include_once "./inc/header.php" ?>
+<?php include_once "./inc/header.php" ?>
+<h1>Home</h1>
+<?php include "./inc/footer.php" ?>
+
+<!-- 
+	include & require 的区别:
+	如果有报错信息,include会显示报错信息,但不影响后面的报错信息
+	如果有报错信息,require会显示报错信息,但后面的代码不会执行
+
+	include_once & require 的作用:
+	重复引用, 只识别一次
+ -->
